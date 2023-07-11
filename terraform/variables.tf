69ae8dbbe5b1c8ba9132dbd841deee30567a5272
Tomas Gomez <tgomez2342@gmail.com>
2023-07-11 17:19:40 -0400

    infra

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "ID of the project you want to use"
+  type        = string
+}
+
+variable "region" {
+  description = "Region of your project"
+  default     = "us-central1"
+}
