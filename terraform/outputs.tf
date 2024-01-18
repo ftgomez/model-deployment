ce142131c65ae832503d3fca91a21cc15f0ed93b
Tomás Gómez <ftgomez@uc.cl>
2024-01-18 15:42:08 -0300

    pre commit

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "enabled_services" {
-  description = "The services that were enabled in this project"
-  value       = [google_project_service.vertexai.service]
-}
-
-output "iam_roles" {
-  description = "The roles that were assigned to the service account"
-  value       = [google_project_iam_binding.vertexai.role, google_project_iam_binding.storage.role]
-}
